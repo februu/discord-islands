a1768953a3879fb6d8ac3f5d16a755fb851bbce2
Kacper <81876955+februu@users.noreply.github.com>
2023-07-26 14:08:29 +0200

    update image_generation.py
    - edit tinyBlocks.png
    - edit bg.png

--- image_generation.py
+++ image_generation.py
@@ -2,62 +2,65 @@ from PIL import Image
 import random
 import time
 
-MAP_X = 7
+
 MAP_Y = 5
-MAP_Z = 7
 TILE_WIDTH = 16
 TILE_HEIGHT = 18
 ISO_TILE_HEIGHT = 8
 
+island = {'id': 175652881456693249,
+          'lastCollectedTimestamp': 1690361813,
+          'balance': 0,
+          'map': [[[-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1]], [[-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1]], [[-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1]]],
+          'upgrades': [],
+          'items': [{'id': 0, 'amount': 1}]}
 
-while True:
+sprites = Image.open("assets/tinyBlocks.png")
+bg = Image.open("assets/bg.png")
 
-    sprites = Image.open("assets/tinyBlocks.png")
-    bg = Image.open("assets/bg.png")
-    map = [[[1, 9, 1, 10, 2, 9, 10], [9, 2, 9, 1, 3, 9, 9], [1, 8, 9, 2, 58, 56, 9], [1, 1, 2, 1, 56, 5, 4], [7, 6, 9, 9, 4, 22, 22], [6, 7, 9, 5, 22, 22, 22], [6, 6, 9, 4, 22, 22, 22]],
-           [[-2, -2, -1, -1, 41, 41, -1], [-2, 30, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1, -1], [-1, -1, -1, -
-                                                                                                       1, -1, -1, -1], [-3, -3, -1, -1, -1, -1, -1], [-3, -3, - 1, -1, -1, -1, -1], [-3, -3, -1, -1, -1, -1, -1]],
-           [[-1, -1, -1, -1, -1, -1, -1], [-1, 30, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1, -1], [-1, -1, -1, -
-                                                                                                       1, -1, -1, -1], [-1, -1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1, -1]],
-           [[-1, 36, -1, -1, -1, -1, -1], [36, 30, 36, -1, -1, -1, -1], [-1, 36, -1, -1, -1, -1, -1], [-1, -1, -1, -
-                                                                                                       1,  -1, -1, -1], [-1, -1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1, -1]],
-           [[-1, -1, -1, -1, -1, -1, -1], [-1, 36, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1, -1], [-1, -1, -1, -
-                                                                                                       1,  -1, -1, -1], [-1, -1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1, -1]], ]
+# Populate with ores.
+for i in range(20):
+    y = random.randint(0, len(island['map']) - 1)
+    x = random.randint(0, len(island['map'][0]) - 1)
+    z = random.randint(0, len(island['map'][0][0]) - 1)
+    while not island['map'][y][z][x] == -1 or y > 0:
+        if island['map'][y-1][z][x] != -1:
+            break
+        y = random.randint(0, len(island['map']) - 1)
+        x = random.randint(0, len(island['map'][0]) - 1)
+        z = random.randint(0, len(island['map'][0][0]) - 1)
+    island['map'][y][z][x] = random.randint(49, 53)
 
-    # Populate with ores.
-    for block in range(5):
-        x = random.randint(0, MAP_X - 1)
-        z = random.randint(0, MAP_Z - 1)
-        while not map[1][z][x] == -1 or map[0][z][x] == 22:
-            x = random.randint(0, MAP_X - 1)
-            z = random.randint(0, MAP_Z - 1)
-        map[1][z][x] = random.randint(49, 53)
+# print(int(time.time()))
 
-    # Populate with crops.
-    for block in range(3):
-        x = random.randint(0, MAP_X - 1)
-        z = random.randint(0, MAP_Z - 1)
-        while not map[1][z][x] == -3:
-            x = random.randint(0, MAP_X - 1)
-            z = random.randint(0, MAP_Z - 1)
-        map[1][z][x] = random.randint(18, 19)
+# Draws the base.
+for z in range(0, len(island['map'][0])):
+    for x in range(0, len(island['map'][0][0])):
+        row, column = divmod(random.choice([9, 9, 9, 10, 11]), 9)
+        tile = sprites.crop((TILE_WIDTH * column, TILE_HEIGHT * row,
+                            TILE_WIDTH * (column + 1), TILE_HEIGHT * (row + 1)))
+        tileX = (x - z) * (TILE_WIDTH / 2)
+        tileY = (x + z) * (ISO_TILE_HEIGHT / 2)
+        # tileY -= y * (ISO_TILE_HEIGHT + 1)
+        bg.paste(tile, (int(tileX) + 56, int(tileY) +
+                 72-len(island['map'])*4), mask=tile)
 
-    # Draws the map.
-    for y in range(0, MAP_Y):
-        for z in range(0, MAP_Z):
-            for x in range(0, MAP_X):
+# Draws blocks.
+for y in range(0,  len(island['map'])):
+    for z in range(0,  len(island['map'][0])):
+        for x in range(0,  len(island['map'][0][0])):
 
-                if not map[y][z][x] == -1:
-                    row, column = divmod(map[y][z][x], 9)
-                    tile = sprites.crop((TILE_WIDTH * column, TILE_HEIGHT * row,
-                                        TILE_WIDTH * (column + 1), TILE_HEIGHT * (row + 1)))
+            if not island['map'][y][z][x] == -1:
+                row, column = divmod(island['map'][y][z][x], 9)
+                tile = sprites.crop((TILE_WIDTH * column, TILE_HEIGHT * row,
+                                    TILE_WIDTH * (column + 1), TILE_HEIGHT * (row + 1)))
 
-                    tileX = (x - z) * (TILE_WIDTH / 2)
-                    tileY = (x + z) * (ISO_TILE_HEIGHT / 2)
-                    tileY -= y * (ISO_TILE_HEIGHT + 1)
-                    bg.paste(tile, (int(tileX) + 56, int(tileY)+48), mask=tile)
+                tileX = (x - z) * (TILE_WIDTH / 2)
+                tileY = (x + z) * (ISO_TILE_HEIGHT / 2)
+                tileY -= (y + 1) * (ISO_TILE_HEIGHT + 1)
+                bg.paste(tile, (int(tileX) + 56, int(tileY) +
+                                72-len(island['map'])*4), mask=tile)
 
-    bg = bg.resize((128*4, 128*4), resample=Image.Resampling.NEAREST)
-    bg.save("image.png", "PNG")
 
-    time.sleep(1)
+bg = bg.resize((128*4, 128*4), resample=Image.Resampling.NEAREST)
+bg.save("image.png", "PNG")
